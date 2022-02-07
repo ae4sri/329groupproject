45955fdaf2ba1d7c6f12faa8a5fa2549a6aee42e
Amin <aminelnasri@hotmail.com>
2022-02-07 15:32:02 -0700

    Add routes for the game, comic, and quiz

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
 
   return ( 
-    <p>329 group project</p>
+    <>
+    <Router>
+        <Routes>
+
+          <Route path="/game" element={<p>Host Game on this Page</p>} />
+
+          <Route path="/comic" element={<p>Put the comic here</p>} />
+
+          <Route path="/quiz" element={<p>Put the quiz here</p>} />
+
+          <Route path="/" element={<p>This is our home page</p>} />
+
+        </Routes>
+      </Router>
+    </>
   )
 }
 
