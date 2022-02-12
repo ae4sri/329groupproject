289a2ba45d6094252757c5827113a77186090b15
Amin <aminelnasri@hotmail.com>
2022-02-12 05:14:08 -0700

    Create a draft quiz

--- src/components/Pages/QuizPage.js
+++ src/components/Pages/QuizPage.js
@@ -1,5 +1,10 @@
+import Quiz from 'react-quiz-component';
+import { Container } from 'react-bootstrap';
+import { quiz } from '../../quiz';
+
+
 export const QuizPage = () => {
   return (
-    <p>quiz page</p>
+      <Quiz quiz={quiz} showInstantFeedback={true} continueTillCorrect={true} />
   )
 }
