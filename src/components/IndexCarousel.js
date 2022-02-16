67964bc94b9545c673b7f38fe6e11756e3e03061
Amin <aminelnasri@hotmail.com>
2022-02-16 06:46:28 -0700

    Use actual photos for Carousel; delete old ones, use some new ones.

--- src/components/IndexCarousel.js
+++ src/components/IndexCarousel.js
@@ -1,57 +1,42 @@
 import Carousel from 'react-bootstrap/Carousel'
-import land1 from '../images/land1.png'
-import land2 from '../images/land2.jpg'
-import land3 from '../images/land3.jpg'
+import codePicture from '../images/codePicture.png'
+import quiz from '../images/quiz.png'
+import comicExample from '../images/comicExample.png'
 
-// eventually, we change these photos with photos of the actual game
 
+//  images should have a width of 1296, height of 500 
 export const IndexCarousel = () => {
-
-  const width = 400
-  const height =  500
 
   return (
     <div className='mb-3'>
       <Carousel>
         <Carousel.Item>
           <img
-            width={width} height={height}
             className="d-block w-100"
-            src={land1}
             alt="First slide"
+            src={comicExample}
           />
-          <Carousel.Caption>
-            <h3>Picture of Game</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+
         </Carousel.Item>
         <Carousel.Item>
           <img
-            width={width} height={height}
-
             className="d-block w-100"
-            src={land2}
+            src={codePicture}
             alt="Second slide"
+            
           />
 
-          <Carousel.Caption>
-            <h3>Picture of a piece of the comic</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+
         </Carousel.Item>
         <Carousel.Item>
           <img
-            width={width} height={height}
 
             className="d-block w-100"
-            src={land3}
+            src={quiz}
             alt="Third slide"
           />
 
-          <Carousel.Caption>
-            <h3>Picture of some of the quiz (maybe)</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
+
         </Carousel.Item>
       </Carousel>
     </div>
