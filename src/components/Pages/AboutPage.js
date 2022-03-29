715baac8e024fa0e8e85815c201d3d699a583591
kravex911 <43700159+kravex911@users.noreply.github.com>
2022-03-29 00:19:45 -0600

    More styling to about page

--- src/components/Pages/AboutPage.js
+++ src/components/Pages/AboutPage.js
@@ -1,31 +1,34 @@
 import { useHref } from "react-router-dom"
 import { Link } from "react-router-dom"
+import { Container, Card } from "react-bootstrap"
+
 
 export const AboutPage = () => {
-  return (
-    <>
-        <b>Who are we?</b>
-        <br />
-        <i>We are students at the University of Calgary. This website is our final project for 
-          CPSC 329 - an informational security course at our University.
-        </i>
-        <br />
-        <br />
-        <b>Our Names</b>
-        <br />
-        <ul>
-          <li>Amin Elnasri</li>
-          <li>Ayo Alabi</li>
-          <li>Umar Hassan</li>
-          <li>Cebrail Durna</li>
-        </ul>
-    <b>Source Code</b>
-    <ul>
-      <li><Link to=".">Website Source Code</Link></li>
-      <br />
-      <li><Link to=".">Game Source Code</Link></li>    
-    </ul>
-    
-    </>
-  )
+    return (
+        <Container>
+            <div style={{marginTop:'30px'}}></div>
+            <b style={{fontSize:'35px', fontWeight:'bolder'}}>Who are we?</b>
+            <br />
+            <i>We are students at the University of Calgary. This website is our final project for
+                CPSC 329 - an informational security course at our University.
+            </i>
+            <br />
+            <br />
+            <b>Our Names</b>
+            <br />
+            <ul>
+                <li>Amin Elnasri</li>
+                <li>Ayo Alabi</li>
+                <li>Umar Hassan</li>
+                <li>Cebrail Durna</li>
+            </ul>
+            <b>Source Code</b>
+            <ul>
+                <li><Link to=".">Website Source Code</Link></li>
+                <br />
+                <li><Link to=".">Game Source Code</Link></li>
+            </ul>
+
+        </Container>
+    )
 }
