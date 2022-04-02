31055426bd81ecbeb3fd421250f27c1310605fc1
Amin <aminelnasri@hotmail.com>
2022-04-02 07:57:10 -0600

    Create a filler Comic page

--- src/components/Pages/ComicPage.js
+++ src/components/Pages/ComicPage.js
@@ -1,5 +1,13 @@
+import { Container } from 'react-bootstrap'
+import comic from '../../images/xkcdfiller.png'
+import Image from 'react-bootstrap/Image'
 export const ComicPage = () => {
   return (
-    <p>comic page</p>
-  )
+    <Container>
+      <div id="aboutTitle" style={{ textAlign: "center" }}> Comic </div>
+      <br />
+      <Image className="mx-auto d-block"  fluid={true} src={comic} />
+      <div id="comicCaption" style={{ textAlign: "center" }}> By Cebrail Durna </div>
+    </Container>
+    )
 }
