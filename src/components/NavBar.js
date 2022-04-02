91e9863b0c59f582fdee9fa74146f95960f3c83a
Amin <aminelnasri@hotmail.com>
2022-04-02 00:16:19 -0600

    Change "About Us" to "About"

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ export const NavBar = () => {
           <Nav.Link as={Link} to="/game" className='nav-link'>Game</Nav.Link>
           <Nav.Link as={Link} to="/comic" className='nav-link'>Comic</Nav.Link>
           <Nav.Link as={Link} to="/quiz" className='nav-link'>Quiz</Nav.Link>
-          <Nav.Link as={Link} to="/about" className='nav-link'>About Us</Nav.Link>
+          <Nav.Link as={Link} to="/about" className='nav-link'>About</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
