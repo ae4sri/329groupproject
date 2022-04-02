34db600230090883c282aebf550151dff5b54e28
Amin <aminelnasri@hotmail.com>
2022-04-02 01:41:42 -0600

    Update index page; change some inline styling

--- src/components/Pages/IndexPage.js
+++ src/components/Pages/IndexPage.js
@@ -8,7 +8,7 @@ export const IndexPage = () => {
 
   return (
     <Container >
-      <h1 style={{ textAlign: "center", marginTop:"20px", fontWeight:"bolder" }}>CPSC 329 W22 Group Project</h1>
+      <h1 class="title" style={{ textAlign: "center", marginTop:"20px", fontWeight:"bolder" }}>Welcome to our Project</h1>
       <hr style={{ width:"90%", margin: "auto"}} />
       <br />
       <IndexCarousel />
@@ -18,18 +18,20 @@ export const IndexPage = () => {
         <Grid item xs={6}>
 
           <Card body>
-            <b style={{fontSize:'20px', fontWeight:'bold'}}>Who are we?</b> <br />
+            <b class="cardTitle">Who are we?</b> 
+            <div class="cardText">
             We are students at the University of Calgary. 
-            To learn more about us, go to the About Us link in the navigation bar.
+            To learn more about us, go to the About Us link in the navigation bar.</div>
+
           </Card>
 
           <br />
 
           <Card body>
 
-            <b style={{fontSize:'20px', fontWeight:'bold'}}>Project Motivation</b> <br />
+            <b class="cardTitle">Our Motivation</b> 
 
-              <div id="example-collapse-text">
+              <div class="cardText">
                 Nowadays, almost every aspect of our lives are connected to the internet one way or another.
                 From our groceries and furniture to our bank accounts and health records. And the trend is that
                 our lives will get more and more digitized as we venture towards the future. As more people get
@@ -41,14 +43,15 @@ export const IndexPage = () => {
                 everyone could reach just by “googling”. In this website, we will convey information about security and
                 privacy to people so that they can learn more about the potential threats they face by just using the internet.
               </div>
-              <div style={{marginTop:'55px'}}></div>
           </Card>
 
         </Grid>
 
         <Grid item xs={6}>
           <Card body>
-            <b style={{fontSize:'20px', fontWeight:'bold'}}>What is this project?</b> <br />
+            <b class="cardTitle">What is this project?</b> 
+            <div class="cardText">
+              
             Our goal was to create an informative website about security and privacy.
             The purpose of this website is to inform users about what we think is vital information about security and privacy
             (topic 1 in the course) in a creative manner. We've tried to make the process of learning enjoyable.
@@ -58,12 +61,14 @@ export const IndexPage = () => {
               <li>A <Link to='/comic'>comic</Link></li>
               <li>A <Link to='/game'>game</Link></li>
             </ul>
+            </div>
           </Card>
 
           <br />
 
           <Card body>
-            <b style={{fontSize:'20px', fontWeight:'bold'}}>What am I supposed to do?</b> <br />
+            <b class="cardTitle">What am I supposed to do?</b> 
+            <div class="cardText">
             We have some basic instructions for you to go through this website.
             <ol>
               <li>First, check out our comic. Do this by going over to the navigation bar, and clicking "Comic".</li>
@@ -71,11 +76,11 @@ export const IndexPage = () => {
               <li>And lastly, we'd like for you to test your knowledge on what you learned from this website, by heading over to do our quiz. Do this by simply going to "Quiz" on the
                 navigation bar.</li>
             </ol>
+            </div>
           </Card>
 
         </Grid>
       </Grid>
-
       <div style={{marginTop:'30px'}}></div>
     </Container>
   )
