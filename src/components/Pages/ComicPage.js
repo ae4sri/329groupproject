29406dd4320565111c3f6453d3015fad4df5dcba
Amin <aminelnasri@hotmail.com>
2022-04-17 11:47:09 -0600

    Add message at bottom of comic

--- src/components/Pages/ComicPage.js
+++ src/components/Pages/ComicPage.js
@@ -12,6 +12,7 @@ import seven from '../../images/comic/7.png'
 import eight from '../../images/comic/8.png'
 
 export const ComicPage = () => {
+
   return (
     <Container>
       <div id="aboutTitle" style={{ textAlign: "center" }}>Comic</div>
@@ -43,6 +44,11 @@ export const ComicPage = () => {
         </Grid>
 
       </Grid>
+      <br />
+      <div id="comicMessage" style={{ textAlign: "center" }}> Would you give your debit to a random stranger
+        claiming to be a bank manager?
+        Would you do the same online?
+        With phishing attacks, adversaries pose as legitemate identities or institutions in order to obtain your sensitive informations such as your e-mail credentials and your credit card information. According to research conducted by FBI, phishing claimed a total of 241,342 victims in 2020.</div>
       <div id="comicCaption" style={{ textAlign: "center" }}> By Cebrail Durna </div>
     </Container>
   )
