db5ce162f884bc47d34da3e81e4a03c7505b1320
Amin <aminelnasri@hotmail.com>
2022-04-17 12:23:24 -0600

    Add file to store HTML for iframe

---
+++ src/comic.js
@@ -0,0 +1,252 @@
+export const comic = `
+<!DOCTYPE html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="http://cmx.io/v/0.1/cmx.css">
+<script src="http://cmx.io/v/0.1/cmx.js" charset="utf-8"></script>
+<style>.cmx-user-scene4 .cmx-text-border .cmx-path {stroke: orange}</style>
+<body>
+  <div style="max-width:900px; -webkit-transform:rotate(0deg)">
+
+    <scene id="scene1">
+      <label t="translate(0,346)">
+        <tspan x="0" y="0em">It was just another day...</tspan>
+      </label>
+      <drawing t="translate(0,31)">
+        <line stroke="green">
+          <point x="0" y="0"></point>
+          <point x="125" y="25"></point>
+          <point x="250" y="20"></point>
+        </line>
+        <line stroke="black">
+          <point x="185" y="20"></point>
+          <point x="185" y="85"></point>
+          <point x="225" y="85"></point>
+          <point x="225" y="20"></point>
+        </line>
+        <line stroke="black">
+          <point x="140" y="20"></point>
+          <point x="140" y="130"></point>
+          <point x="250" y="130"></point>
+        </line>
+        <line stroke="red">
+          <point x="140" y="130"></point>
+          <point x="205" y="170"></point>
+          <point x="250" y="150"></point>
+        </line>
+  
+      </drawing>
+      <actor t="translate(71,19) rotate(-2)" pose="-11,9|-5,117|-11,99|-11,89|-11,79|-11,59|-16,34|-21,9|-6,34|-1,9|-18,79|-18,59|-6,79|-1,59">
+        <bubble t="translate(9,11)" pose="0,0|-20,10|-77,60|19,66|-21,145|-76,125">
+          <tspan x="0" y="-3em">Hey, I'm from the bank.</tspan>
+          <tspan x="0" y="0em">Thank god I was able to</tspan>
+          <tspan x="0" y="1em">find you! There's a matter</tspan>
+          <tspan x="0" y="2em">requiring immediate</tspan>
+          <tspan x="0" y="3em">action for your account!</tspan>
+        </bubble>
+      </actor>
+      <actor t="translate(159,15)" pose="2,5|4,113|2,95|2,85|2,75|2,55|-8,35|-8,5|7,30|12,5|-8,76|-29,72|12,75|12,55">
+        <bubble t="translate(-2,-9)" pose="0,0|-14,24|-36,69|27,28|8,72|-61,78">
+          <tspan x="0" y="0em">Wait, what?!?!?</tspan>
+          <tspan x="0" y="1em">Who are you?</tspan>
+        </bubble>
+      </actor>
+    </scene>
+
+    <scene id="scene2">
+      <drawing t="translate(0,31)">
+        <line stroke="green">
+          <point x="0" y="0"></point>
+          <point x="125" y="25"></point>
+          <point x="250" y="20"></point>
+        </line>
+        <line stroke="black">
+          <point x="185" y="20"></point>
+          <point x="185" y="85"></point>
+          <point x="225" y="85"></point>
+          <point x="225" y="20"></point>
+        </line>
+        <line stroke="black">
+          <point x="140" y="20"></point>
+          <point x="140" y="130"></point>
+          <point x="250" y="130"></point>
+        </line>
+        <line stroke="red">
+          <point x="140" y="130"></point>
+          <point x="205" y="170"></point>
+          <point x="250" y="150"></point>
+        </line>
+  
+      </drawing>
+      <actor t="translate(71,19) rotate(-2)" pose="-11,9|-1,114|-11,99|-11,89|-11,79|-11,59|-16,34|-21,9|-6,34|-1,9|-18,79|-18,59|-6,79|13,83">
+        <bubble t="translate(-3,0)" pose="0,0|-12,22|-83,104|45,21|-37,182|-58,162">
+          <tspan x="0" y="-2em">I am the manager of the Bank!
+          <tspan x="0" y="0em">Earlier in the day I realized</tspan>
+          <tspan x="0" y="1em">some irregularities with your</tspan>
+          <tspan x="0" y="2em">account.</tspan>
+          <tspan x="0" y="8em">Yeah, duh....</tspan>
+        </tspan></bubble>
+      </actor>
+      <actor t="translate(159,15)" pose="15,8|17,116|15,98|15,88|15,78|15,58|5,38|0,8|20,33|25,8|-5,96|1,121|25,78|25,58">
+        <bubble t="translate(-2,-9)" pose="0,0|7,33|14,66|-56,54|-45,86|-94,113">
+          <tspan x="0" y="1em">My bank account?</tspan>
+        </bubble>
+      </actor>
+    </scene>
+
+    <scene id="scene3">
+      <actor t="translate(209,4) rotate(2)" pose="-172,67|-120,126|-131,107|-131,97|-132,90|-131,67|-136,42|-141,17|-126,42|-121,17|-133,90|-126,74|-112,83|-100,75">
+        <bubble t="translate(-2,-9) rotate(-4)" pose="0,18|-2,27|-18,55|93,-4|75,42|-38,75">
+          <tspan x="0" y="-4em">It was just like any other </tspan>
+          <tspan x="0" y="-3em">morning, then one of my </tspan>
+          <tspan x="0" y="-2em">employees spotted it...</tspan>
+          <tspan x="0" y="-1em">Apparently some Russian</tspan>
+          <tspan x="0" y="0em">Hacker Got Into Your</tspan>
+          <tspan x="0" y="1em">Account...</tspan>
+        </bubble>
+      </actor>
+    </scene>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <scene id="scene1">
+      <drawing t="translate(0,31)">
+        <line stroke="green">
+          <point x="0" y="0"></point>
+          <point x="125" y="25"></point>
+          <point x="250" y="20"></point>
+        </line>
+        <line stroke="black">
+          <point x="185" y="20"></point>
+          <point x="185" y="85"></point>
+          <point x="225" y="85"></point>
+          <point x="225" y="20"></point>
+        </line>
+        <line stroke="black">
+          <point x="140" y="20"></point>
+          <point x="140" y="130"></point>
+          <point x="250" y="130"></point>
+        </line>
+        <line stroke="red">
+          <point x="140" y="130"></point>
+          <point x="205" y="170"></point>
+          <point x="250" y="150"></point>
+        </line>
+  
+      </drawing>
+      <actor t="translate(71,19) rotate(-2)" pose="-11,9|-5,117|-11,99|-11,89|-11,79|-11,59|-16,34|-21,9|-6,34|-1,9|-18,79|-18,59|-6,79|-1,59">
+        <bubble t="translate(9,11)" pose="0,0|-20,10|-77,60|16,5|-24,84|-64,71">
+          <tspan x="0" y="-1em">Relax, relax</tspan>
+          <tspan x="0" y="1em">Everything is OK</tspan>
+          <tspan x="0" y="2em">For now...</tspan>
+        </bubble>
+      </actor>
+      <actor t="translate(159,15)" pose="2,5|4,113|2,95|2,85|2,75|2,55|-8,35|-8,5|7,30|12,5|-8,76|-29,72|12,75|12,55">
+        <bubble t="translate(-2,-9)" pose="0,0|-14,24|-36,69|45,133|26,177|-32,144">
+          <tspan x="0" y="-3em">Russians?!?!?!?!</tspan>
+          <tspan x="0" y="0em">I'm screwed.'</tspan>
+          <tspan x="0" y="2em">What am I</tspan>
+          <tspan x="0" y="3em">gonna do?</tspan>
+        </bubble>
+      </actor>
+    </scene>
+
+    <scene id="scene2">
+      <drawing t="translate(0,31)">
+        <line stroke="green">
+          <point x="0" y="0"></point>
+          <point x="125" y="25"></point>
+          <point x="250" y="20"></point>
+        </line>
+        <line stroke="black">
+          <point x="185" y="20"></point>
+          <point x="185" y="85"></point>
+          <point x="225" y="85"></point>
+          <point x="225" y="20"></point>
+        </line>
+        <line stroke="black">
+          <point x="140" y="20"></point>
+          <point x="140" y="130"></point>
+          <point x="250" y="130"></point>
+        </line>
+        <line stroke="red">
+          <point x="140" y="130"></point>
+          <point x="205" y="170"></point>
+          <point x="250" y="150"></point>
+        </line>
+  
+      </drawing>
+      <actor t="translate(71,19) rotate(-2)" pose="-11,9|-7,116|-11,99|-11,89|-11,79|-11,59|-16,34|-21,9|-6,34|-1,9|-18,79|-12,65|0,81|-10,62">
+        <bubble t="translate(-3,0)" pose="0,0|-12,22|-83,104|45,21|-37,182|-58,162">
+          <tspan x="0" y="-1em">As long as you give me
+          <tspan x="0" y="0em">authorization, I can do what</tspan>
+          <tspan x="0" y="1em">is necessary to secure your</tspan>
+          <tspan x="0" y="2em">account.</tspan>
+          <tspan x="0" y="8em">Thx, it's my job</tspan>
+        </tspan></bubble>
+      </actor>
+      <actor t="translate(159,15)" pose="15,8|17,116|15,98|15,88|15,78|15,58|5,38|0,8|20,33|25,8|-5,113|18,120|25,78|25,58">
+        <bubble t="translate(-2,-9)" pose="0,0|7,33|14,66|-56,54|-45,86|-73,125">
+          <tspan x="0" y="1em">Sure, I guess that</tspan>
+          <tspan x="0" y="2em">makes sense...</tspan>
+        </bubble>
+      </actor>
+    </scene>
+    
+    <scene id="scene1">
+      <drawing t="translate(0,31)">
+        <line stroke="green">
+          <point x="0" y="0"></point>
+          <point x="125" y="25"></point>
+          <point x="250" y="20"></point>
+        </line>
+        <line stroke="black">
+          <point x="185" y="20"></point>
+          <point x="185" y="85"></point>
+          <point x="225" y="85"></point>
+          <point x="225" y="20"></point>
+        </line>
+        <line stroke="black">
+          <point x="140" y="20"></point>
+          <point x="140" y="130"></point>
+          <point x="250" y="130"></point>
+        </line>
+        <line stroke="red">
+          <point x="140" y="130"></point>
+          <point x="205" y="170"></point>
+          <point x="250" y="150"></point>
+        </line>
+  
+      </drawing>
+      <actor t="translate(71,19) rotate(-2)" pose="-11,9|-5,117|-11,99|-11,89|-11,79|-11,59|-16,34|-21,9|-6,34|-1,9|-18,79|-18,59|-6,79|-1,59">
+        <bubble t="translate(9,11)" pose="0,0|-20,10|-77,60|9,99|-31,178|-72,142">
+          <tspan x="0" y="-3em">Just need your card number</tspan>
+          <tspan x="0" y="-2em">to confirm your Identity'</tspan>
+          <tspan x="0" y="2em">And finally, your cvv</tspan>
+          <tspan x="0" y="3em">and expiration date?</tspan>
+        </bubble>
+      </actor>
+      <actor t="translate(159,15)" pose="2,5|4,113|2,95|2,85|2,75|2,55|-8,35|-8,5|7,30|12,5|-8,76|-29,72|12,75|12,55">
+        <bubble t="translate(-2,-9)" pose="0,0|-14,24|-36,69|45,133|26,177|-63,111">
+          <tspan x="0" y="-4em">Ofcourse, 5724***</tspan>
+          <tspan x="0" y="1em">*** and **/**</tspan>
+          <tspan x="0" y="2em">Wheeew, that was a</tspan>
+          <tspan x="0" y="3em">close call,  thanks!</tspan>
+        </bubble>
+      </actor>
+    </scene>
+        <scene id="scene3" height="230">
+      <actor t="translate(209,4) rotate(2)" pose="-139,51|-95,108|-98,91|-98,81|-99,74|-98,51|-103,26|-108,1|-93,26|-88,1|-113,80|-108,94|-83,77|-84,98">
+        <bubble t="translate(-2,-9) rotate(-4)" pose="0,18|-2,27|-18,55|-38,-4|-56,42|-89,78">
+
+      <label t="translate(-103,122) rotate(1)" pose="0,0|-2,-9">
+        <tspan x="0" y="0em">Later in the day...</tspan>
+      </label>
+
+          <tspan x="0" y="0em">For some reason my bank </tspan>
+          <tspan x="0" y="1em">balance shows as empty...</tspan>
+          <tspan x="0" y="2em">Must be to fool the Russians!</tspan>
+        </bubble>
+      </actor>
+    </scene>
+    
+  </div></body>
+`
