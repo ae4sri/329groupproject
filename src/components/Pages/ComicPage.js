79ee1655afb903baf8ee9f0dd62041940cf458ef
Amin <aminelnasri@hotmail.com>
2022-04-17 12:23:38 -0600

    Use an iframe to display the comic rather than grid

--- src/components/Pages/ComicPage.js
+++ src/components/Pages/ComicPage.js
@@ -1,50 +1,15 @@
 import { Container } from 'react-bootstrap'
-import comic from '../../images/xkcdfiller.png'
-import Image from 'react-bootstrap/Image'
-import { Grid, Item } from '@mui/material'
-import one from '../../images/comic/1.png'
-import two from '../../images/comic/2.png'
-import three from '../../images/comic/3.png'
-import four from '../../images/comic/4.png'
-import five from '../../images/comic/5.png'
-import six from '../../images/comic/6.png'
-import seven from '../../images/comic/7.png'
-import eight from '../../images/comic/8.png'
+import { comic } from '../../comic'
 
 export const ComicPage = () => {
 
   return (
     <Container>
       <div id="aboutTitle" style={{ textAlign: "center" }}>Comic</div>
-      <br />
-      <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <img className="mx-auto d-block" height={400} width={350} fluid={true} src={one} />
-        </Grid>
-        <Grid item xs={4}>
-          <img className="mx-auto d-block" height={400} width={350} fluid={true} src={two} />
-        </Grid>
-        <Grid item xs={4}>
-          <img className="mx-auto d-block" height={400} width={350} fluid={true} src={three} />
-        </Grid>
-        <Grid item xs={6}>
-          <img className="mx-auto d-block" height={400} width={400} fluid={true} src={four} />
-        </Grid>
-        <Grid item xs={6}>
-          <img className="mx-auto d-block" height={400} width={350} fluid={true} src={five} />
-        </Grid>
-        <Grid item xs={4}>
-          <img className="mx-auto d-block" height={400} width={350} fluid={true} src={six} />
-        </Grid>
-        <Grid item xs={4}>
-          <img className="mx-auto d-block" height={400} width={350} fluid={true} src={seven} />
-        </Grid>
-        <Grid item xs={4}>
-          <img className="mx-auto d-block" height={400} width={350} fluid={true} src={eight} />
-        </Grid>
-
-      </Grid>
-      <br />
+      <div id="warning" style={{ textAlign: "center" }}>(if it doesn't render properly, refresh your page!) </div>
+      <div id="iframe">
+        <iframe width="1000" height="1100" srcdoc={comic} />
+      </div>
       <div id="comicMessage" style={{ textAlign: "center" }}> Would you give your debit to a random stranger
         claiming to be a bank manager?
         Would you do the same online?
